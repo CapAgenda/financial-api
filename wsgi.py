376e551ff0419e20d04664cc3f990f7e5a7d18a7
CapAgenda <capitalistagenda@pm.me>
2022-10-04 16:25:25 -0700

    hello

--- wsgi.py
+++ wsgi.py
@@ -8,13 +8,7 @@ def application(environ, start_response):
     if path == "/test":
         start_response('200 OK', [('Content-Type', 'text/plain')])
         yield bytes(f"in dev: path/user agent:|{path}|{ua}|\n",encoding='utf8')
-    elif path == "/jump":
-        if "url" in request.params:
-          jump_url = request.params["url"]
-        else:
-          jump_url = "URL_NOT_FOUND"    
-        start_response('200 OK', [('Content-Type', 'text/html')])
-        yield bytes(jump.jump_html(jump_url),encoding='utf8') 
+ 
     else:
         start_response('200 OK', [('Content-Type', 'text/html')])
         yield bytes(f"hello world",encoding='utf8') 
